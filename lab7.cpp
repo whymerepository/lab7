140f328966e6635b796bd03626f3af232c61b4f2
whymerepository <71703045+whymerepository@users.noreply.github.com>
2020-10-21 20:42:41 +0300

    Add files via upload

---
+++ lab7.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+#include <malloc.h>
+
+int main()
+{
+	char* str;
+	int length, pos1 = -1, pos2 = -1;
+
+	printf("Str length: ");
+	scanf_s("%d", &length);
+	getchar();
+
+	str = (char*)calloc(sizeof(char), length + 2);
+	fgets(str, length+1, stdin);
+
+
+	for (int i = 0; i < length; i++)
+		if (str[i] == ':')
+		{
+			pos1 = i + 1;
+			break;
+		}
+
+
+	for (int i = pos1; i < length; i++)
+		if (str[i] == ':')
+			pos2 = i;
+
+	printf("%d %d ", pos1, pos2);
+
+	if (pos2 > 0)
+		for (int i = pos1; i < pos2; i++)
+			printf("%c", *(str + i));
+	else
+		for (int i = pos1; i < length; i++)
+			printf("%c", *(str + i));
+
+	return 0;
+}
